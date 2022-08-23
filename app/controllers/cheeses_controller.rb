9588a4b6b93793a72789d2d3182cdf54699af4c6
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-23 14:58:11 -0400

    complete assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    render json: Cheese.find(params[:id])
+  end
+
+  def price_above
+    render json: Cheese.all.select { |c| c.price >= params[:price].to_i }
+  end
 end
